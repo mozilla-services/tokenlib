18994ba9967e877f07a95a7cbf9f937fbddd2c42
Ryan Kelly <rkelly@mozilla.com>
2014-01-08 20:26:21 +1100

    Bump dev version number.

--- tokenlib/__init__.py
+++ tokenlib/__init__.py
@@ -9,8 +9,8 @@ Generic library for managing signed authentication tokens.
 
 __ver_major__ = 0
 __ver_minor__ = 3
-__ver_patch__ = 0
-__ver_sub__ = ""
+__ver_patch__ = 1
+__ver_sub__ = ".dev1"
 __ver_tuple__ = (__ver_major__, __ver_minor__, __ver_patch__, __ver_sub__)
 __version__ = "%d.%d.%d%s" % __ver_tuple__
 
